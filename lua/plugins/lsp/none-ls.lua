a07c3d42f2442b27812739e8f97253aedb9ac649
Brianstein27 <brianstein27@proton.me>
2025-01-24 23:52:38 +0100

    Fix lspconfig

--- lua/plugins/lsp/none-ls.lua
+++ lua/plugins/lsp/none-ls.lua
@@ -1,17 +1,31 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-        null_ls.builtins.formatting.rustywind,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.erb_format,
-				null_ls.builtins.diagnostics.erb_lint,
-			},
-		})
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.black.with({
+          extra_args = { "--line--length=120" }
+        }),
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.asmft,
+        null_ls.builtins.diagnostics.pylint.with({
+          diagnostics_postprocess = function(diagnostic)
+            diagnostic.code = diagnostic.message_id
+          end,
+        }),
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.clang_format,
+        null_ls.builtins.formatting.htmlbeautifier,
+        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.formatting.goimports_reviser,
+        null_ls.builtins.diagnostics.cppcheck,
+        null_ls.builtins.diagnostics.golangci_lint,
+        null_ls.builtins.diagnostics.zsh,
+      },
+    })
 
-		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
+  end,
 }
